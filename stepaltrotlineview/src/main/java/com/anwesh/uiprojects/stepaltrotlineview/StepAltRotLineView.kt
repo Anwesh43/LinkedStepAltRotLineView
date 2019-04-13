475b0052351b1e7c7b97ce27c46c81333a388184
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 16:44:38 +0530

    adding Animator class

--- stepaltrotlineview/src/main/java/com/anwesh/uiprojects/stepaltrotlineview/StepAltRotLineView.kt
+++ stepaltrotlineview/src/main/java/com/anwesh/uiprojects/stepaltrotlineview/StepAltRotLineView.kt
@@ -102,4 +102,32 @@ class StepAltRotLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View) {
+
+        private var animated : Boolean = false
+
+        fun animate(cb : () -> Unit) {
+            cb()
+            try {
+                Thread.sleep(50)
+                view.invalidate()
+            } catch(ex : Exception) {
+
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
