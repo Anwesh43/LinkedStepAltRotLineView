02f9d708ca1d689e79bf30a90f53e4eba538ba7c
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 16:58:50 +0530

    adding view in activity + fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedstepaltrotlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepaltrotlineview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedstepaltrotlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.stepaltrotlineview.StepAltRotLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepAltRotLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
