70c318d2c59d86f95ee662211efb8badae35814e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 16:40:53 +0530

    adding StepAltRotLineView class

--- stepaltrotlineview/src/main/java/com/anwesh/uiprojects/stepaltrotlineview/StepAltRotLineView.kt
+++ stepaltrotlineview/src/main/java/com/anwesh/uiprojects/stepaltrotlineview/StepAltRotLineView.kt
@@ -65,3 +65,21 @@ fun Canvas.drawSARLNode(i : Int, scale : Float, paint : Paint) {
     restore()
     restore()
 }
+
+class StepAltRotLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
