b6643c07e8f1de9ce211bfbf70587a921383f40d
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 16:28:16 +0530

    adding libraries and constants

--- stepaltrotlineview/src/main/java/com/anwesh/uiprojects/stepaltrotlineview/StepAltRotLineView.kt
+++ stepaltrotlineview/src/main/java/com/anwesh/uiprojects/stepaltrotlineview/StepAltRotLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.stepaltrotlineview
 /**
  * Created by anweshmishra on 13/04/19.
  */
+
+import android.view.View
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val lineDeg : Float = 90f
